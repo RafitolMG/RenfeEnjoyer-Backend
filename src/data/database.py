7336f016921e70d4c122e931a075ca75b7c4e374
Rafael Molina García <rafmolgar2@alum.us.es>
2025-02-05 21:08:22 +0100

    feat: completily functional database

--- src/data/database.py
+++ src/data/database.py
@@ -1,50 +1,62 @@
 import sqlite3
-from src.templates import modals
+from src.templates.modals import modals
+
 
 def create_database():
+    global conn
     try:
         conn = sqlite3.connect('renfe_enjoyer_database.db')
         c = conn.cursor()
+        c.execute('''DROP TABLE IF EXISTS users''')
         c.execute('''CREATE TABLE IF NOT EXISTS users
                      (id INTEGER PRIMARY KEY, 
                      username TEXT NOT NULL, 
                      password TEXT NOT NULL,
-                     email TEXT NOT NULL CHECK (email LIKE '%@%.%'), 
+                     email TEXT NOT NULL, 
                      abono TEXT NOT NULL)''')
 
         conn.commit()
         conn.close()
 
+
         modals.show_success('Database created successfully')
 
     except Exception as e:
+        conn.close()
         modals.show_error(str(e))
+
 
 def add_user(username, password, email, abono):
     try:
         conn = sqlite3.connect('renfe_enjoyer_database.db')
         c = conn.cursor()
-        c.execute("INSERT INTO users (username, password, email, abono) VALUES (?, ?, ?, ?)", (username, password, email, abono))
+        c.execute("INSERT INTO users (username, password, email, abono) VALUES (?, ?, ?, ?)",
+                  (username, password, email, abono))
         conn.commit()
         conn.close()
 
         modals.show_success('User added successfully')
 
     except Exception as e:
+        conn.close()
         modals.show_error(str(e))
+
 
 def edit_user(username, password, email, abono):
     try:
         conn = sqlite3.connect('renfe_enjoyer_database.db')
         c = conn.cursor()
-        c.execute("UPDATE users SET username=?, password=?, email=?, abono=? WHERE username=?", (username, password, email, abono, username))
+        c.execute("UPDATE users SET username=?, password=?, email=?, abono=? WHERE username=?",
+                  (username, password, email, abono, username))
         conn.commit()
         conn.close()
 
         modals.show_success('User edited successfully')
 
     except Exception as e:
+        conn.close()
         modals.show_error(str(e))
+
 
 def get_all_users():
     try:
@@ -57,6 +69,21 @@ def get_all_users():
         return users
 
     except Exception as e:
+        conn.close()
+        modals.show_error(str(e))
+
+def get_user(username):
+    try:
+        conn = sqlite3.connect('renfe_enjoyer_database.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM users WHERE username=?", (username,))
+        user = c.fetchone()
+        conn.close()
+
+        return user
+
+    except Exception as e:
+        conn.close()
         modals.show_error(str(e))
 
 def delete_user(username):
@@ -70,4 +97,5 @@ def delete_user(username):
         modals.show_success('User deleted successfully')
 
     except Exception as e:
+        conn.close()
         modals.show_error(str(e))
---
+++ src/templates/views/user_selection.py
@@ -0,0 +1,74 @@
+import tkinter as tk
+from tkinter import ttk
+
+from src.data.database import get_all_users, get_user, edit_user, delete_user
+
+
+def user_selection_view(set_current_user):
+    def on_select():
+        set_current_user.set(user.get())
+        root.destroy()
+
+    def on_edit():
+        curr_user= user.get()
+        curr_user= get_user(curr_user)
+        v= tk.Toplevel()
+        v.title("Editar datos")
+        v.geometry("600x400")
+        v.wm_iconbitmap('./assets/u327as.ico')
+        user_label = ttk.Label(v, text="Usuario:")
+        user_label.pack()
+        user_entry = ttk.Entry(v)
+        user_entry.insert(-1, curr_user[1])
+        user_entry.pack()
+
+        mail_label = ttk.Label(v, text="Correo:")
+        mail_label.pack()
+        mail_entry = ttk.Entry(v)
+        mail_entry.insert(-1, curr_user[2])
+        mail_entry.pack()
+
+        ctr_label = ttk.Label(v, text="Contraseña:")
+        ctr_label.pack()
+        ctr_entry = ttk.Entry(v)
+        ctr_entry.insert(-1, curr_user[3])
+        ctr_entry.pack()
+
+        abono_label = ttk.Label(v, text="Abono:")
+        abono_label.pack()
+        abono_entry = ttk.Entry(v)
+        abono_entry.insert(-1, curr_user[4])
+        abono_entry.pack()
+        def edit_on():
+            edit_user(str(user_entry.get()), str(mail_entry.get()), str(ctr_entry.get()), str(abono_entry.get()))
+            v.destroy()
+        submit_button = ttk.Button(v, text="Guardar",
+                                   command=lambda: edit_on())
+        submit_button.pack()
+
+    def on_delete():
+        curr_user= user.get()
+        delete_user(curr_user)
+        users_data= get_all_users()
+        root.destroy()
+        user_selection_view(set_current_user)
+
+    root = tk.Toplevel()
+    root.title("User Selection")
+    root.geometry("300x200")
+    root.wm_iconbitmap('./assets/u327as.ico')
+    users_data = get_all_users()
+    label = ttk.Label(root, text="Select a user:")
+    label.pack()
+
+    user = ttk.Combobox(root)
+    user['values'] = users_data
+    user.pack()
+    edit_button = ttk.Button(root, text="Edit", command=on_edit)
+    edit_button.pack()
+
+    delete_button = ttk.Button(root, text="Delete", command=on_delete)
+    delete_button.pack()
+
+    button = ttk.Button(root, text="Select", command=on_select)
+    button.pack()
